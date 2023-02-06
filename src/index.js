a0ae451c9da288ee22656cc93a59743316df76df
bryanalay <bryanalmi27@hotmail.com>
2023-02-06 12:59:02 -0500

    fix port

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import cors from 'cors'
 import { router } from "./router.js";
 
 const app = express()
-
+const PORT = 3050 ||process.env.PORT;
+0 
 app.use(cors())
 app.use(express.json())
 
 router(app)
 
-app.listen(3000 ||process.env.PORT,()=>{
-    console.log(`Running ${process.env.PORT || 3000}`);
+app.listen(PORT,()=>{
+    console.log('Running at port ' ,`${PORT}`);
 })
