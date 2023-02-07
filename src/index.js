07addfab278ea4dc985d6c25ae2eefcfacc1f9ff
bryanalay <bryanalmi27@hotmail.com>
2023-02-07 00:38:48 -0500

    fx

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import cors from 'cors'
 import { router } from "./router.js";
 
 const app = express()
-const PORT = 3050 ||process.env.PORT;
+const port = process.env.PORT||3000;
 app.use(cors())
 app.use(express.json())
 
 router(app)
 
-app.listen(PORT,()=>{
-    console.log('Running at port ',`${PORT}`);
+app.listen(port,()=>{
+    console.log('Running at port ',`${port}`);
 })
