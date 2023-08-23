7a4e03203576ce85017f4a20eec881193e3dccec
bryanalay <bryanalmi27@hotmail.com>
2023-08-23 16:22:24 -0500

    fix order posts

--- src/db/postQuery.js
+++ src/db/postQuery.js
@@ -1,44 +1,44 @@
-import { query } from "../config/db.js";
+import { query } from '../config/db.js'
 
 const postQuery = {
   getPosts: async () => {
-    const qre = "select * FROM getPosts();";
-    const result = await query(qre);
-    return result[1].rows;
+    const qre = 'select * FROM getPosts() order by fecha desc;'
+    const result = await query(qre)
+    return result[1].rows
   },
 
   getPostById: async (id) => {
-    const qre = `select * from postdb where id = '${id}'`;
-    const result = await query(qre);
-    return result[1].rows;
+    const qre = `select * from postdb where id = '${id}'`
+    const result = await query(qre)
+    return result[1].rows
   },
 
   getPostByUserId: async (id) => {
-    const qre = `select id, body, fecha from postdb where user_id = '${id}';`;
-    const result = await query(qre);
-    return result[1].rows;
+    const qre = `select id, body, fecha from postdb where user_id = '${id}';`
+    const result = await query(qre)
+    return result[1].rows
   },
 
   getUsernamesByPosyId: async (id) => {
-    const qre = `select * FROM getLikesByPostId('${id}');`;
-    const result = await query(qre);
-    return result[1].rows;
+    const qre = `select * FROM getLikesByPostId('${id}');`
+    const result = await query(qre)
+    return result[1].rows
   },
 
   insertPost: async (post) => {
-    const { id, userid, data, fecha } = post;
+    const { id, userid, data, fecha } = post
     const qre = `INSERT INTO postdb (id,user_id, body,fecha)
   VALUES ('${id}','${userid}','${data}','${fecha}')
-  RETURNING (SELECT username FROM userdb WHERE id = '${userid}');`;
-    const result = await query(qre);
-    return result[1];
+  RETURNING (SELECT username FROM userdb WHERE id = '${userid}');`
+    const result = await query(qre)
+    return result[1]
   },
 
   deletePost: async (id) => {
-    const qre = `delete from postdb where id = '${id}';`;
-    const result = await query(qre);
-    return result[1];
-  },
-};
+    const qre = `delete from postdb where id = '${id}';`
+    const result = await query(qre)
+    return result[1]
+  }
+}
 
-export default postQuery;
+export default postQuery
--- src/router.js
+++ src/router.js
@@ -1,25 +1,24 @@
-import express from "express";
-import { userRoutes } from "./routes/userRoutes.js";
-import { saltRoutes } from "./routes/saltRoutes.js";
-import { loginRoutes } from "./routes/loginRoutes.js";
-import { postsRoutes } from "./routes/postsRoutes.js";
-import { likeRoutes } from "./routes/likeRoutes.js";
-import { authenticateJWT } from "./auth/index.js";
-import { sensorRoutes } from "./routes/sensorRoutes.js";
-import { homeRoutes } from "./routes/homeRoutes.js";
+import express from 'express'
+import { userRoutes } from './routes/userRoutes.js'
+import { saltRoutes } from './routes/saltRoutes.js'
+import { loginRoutes } from './routes/loginRoutes.js'
+import { postsRoutes } from './routes/postsRoutes.js'
+import { likeRoutes } from './routes/likeRoutes.js'
+import { authenticateJWT } from './auth/index.js'
+import { sensorRoutes } from './routes/sensorRoutes.js'
+import { homeRoutes } from './routes/homeRoutes.js'
 
 const router = (app) => {
-
-  const route = express.Router();
-  const routev2 = express.Router();
-  app.use("/api/v1", route);
-  route.use("/", homeRoutes)
-  route.use("/login", loginRoutes);
-  route.use("/user", userRoutes);
-  route.use("/posts", authenticateJWT, postsRoutes);
-  route.use("/like",authenticateJWT, likeRoutes);
-  route.use("/sensor", sensorRoutes);
+  const route = express.Router()
+  const routev2 = express.Router()
+  app.use('/api/v1', route)
+  route.use('/', homeRoutes)
+  route.use('/login', loginRoutes)
+  route.use('/user', userRoutes)
+  route.use('/posts', authenticateJWT, postsRoutes)
+  route.use('/like', authenticateJWT, likeRoutes)
+  route.use('/sensor', sensorRoutes)
   //route.use("/salt", authenticateJWT, saltRoutes);
-};
+}
 
-export { router };
+export { router }
